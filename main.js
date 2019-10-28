7f8590c438283f752d5782efb557adf2838040e9
alexbryw <alexbryw@gmail.com>
2019-10-28 15:00:33 +0100

    new main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+function hej(){
+    if(document.body.querySelector('header').style.height != '100vh'){
+        document.body.querySelector('header').style.height = '100vh';
+    }
+    else{
+        document.body.querySelector('header').style.height = '5rem';
+    }
+    
+}
