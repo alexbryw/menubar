3f6b97ba7c4afabf24ee982f585138817cd217c1
alexbryw <alexbryw@gmail.com>
2019-10-29 10:35:57 +0100

    Js hide drop-down text

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function hej(){
     if(document.body.querySelector('header').style.height != '100vh'){
         document.body.querySelector('header').style.height = '100vh';
+        document.body.querySelector('.drop-down').style.display = 'initial'
     }
     else{
         document.body.querySelector('header').style.height = '5rem';
+        document.body.querySelector('.drop-down').style.display = 'none'
     }
     
 }
