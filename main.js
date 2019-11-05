40cb6505fd245862cde29c42548b2c6a136a4fc3
alexbryw <alexbryw@gmail.com>
2019-11-05 16:06:26 +0100

    Added extra nav container, and a display none on dropdown.

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function hej(){
         document.body.querySelector('.drop-down').style.display = 'initial'
     }
     else{
-        document.body.querySelector('header').style.height = '5rem';
+        document.body.querySelector('header').style.height = '4rem';
         document.body.querySelector('.drop-down').style.display = 'none'
     }
     
