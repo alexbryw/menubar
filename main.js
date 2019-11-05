57776a80759cc30f879a571530076e20331f56df
alexbryw <alexbryw@gmail.com>
2019-11-05 17:05:11 +0100

    Js eventListener detects if screen size changed and removes drop down menu.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-function hej(){
+function dropDown(){
     if(document.body.querySelector('header').style.height != '100vh'){
         document.body.querySelector('header').style.height = '100vh';
         document.body.querySelector('.drop-down').style.display = 'initial'
@@ -8,4 +8,14 @@ function hej(){
         document.body.querySelector('.drop-down').style.display = 'none'
     }
     
+}
+//Event listener for screen resize.
+window.addEventListener("resize", removeDropDown);
+
+
+//remove dropdown if its down and the screen is lager than 720px.
+function removeDropDown(){
+    if(document.body.querySelector('header').style.height == '100vh' && window.innerWidth >= 720){
+        document.body.querySelector('header').style.height = '4rem';
+    }
 }
